e7cb0e127e2201de0f4e6deeb5d53023be29a81b
andrey.zaycev <dondrakone89@mail.ru>
2017-09-26 01:47:42 +0300

    dwf

---
+++ SpaceTruck/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField]
+    private int damageValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public int GetDamage()
+    {
+        return damageValue;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SpaceTruck/Assets/Scripts/DestroyedObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedObj : MonoBehaviour {
+
+    [SerializeField]
+    private int health;
+
+
+    public void Damage(int value)
+    {
+        health -= value;
+        if (health <= 0) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            Bullet b = other.gameObject.GetComponent<Bullet>();
+            Damage(b.GetDamage());
+        }
+    }
+}
